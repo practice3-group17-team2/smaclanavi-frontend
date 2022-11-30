5cdec733883593db1e6ab575568b1ba3d38421a0
littleswampman <littleswampman@gmail.com>
2022-11-30 15:14:08 +0900

    create hooks/

---
+++ src/hooks/api/useVenueApi.ts
@@ -0,0 +1,41 @@
+import assert from 'assert';
+import { useQuery } from '@tanstack/react-query';
+
+export const useVenueApi = <
+  QueryParams extends Record<never, never>,
+  Response extends Record<never, never>,
+>(
+  key: string,
+  path: string,
+  params: QueryParams,
+  selector: (data: Response) => Response = (data) => data,
+) => {
+  const API_ROOT = process.env.NEXT_PUBLIC_API_ROOT;
+  assert(API_ROOT, 'API_ROOT is not defined');
+
+  const fetcher = async () => {
+    const query = new URLSearchParams(params).toString();
+    const url = `${API_ROOT}${path}${query ? `?${query}` : ''}`;
+
+    const response = await fetch(url, { method: 'GET' });
+    assert(response.status === 200, response.status.toString());
+
+    return response.json();
+  };
+
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+  }: {
+    data: Response | undefined;
+    isLoading: boolean;
+    isError: boolean;
+    error: Error | null;
+  } = useQuery([key, params], fetcher, {
+    select: selector,
+  });
+
+  return { data, isLoading, isError, error };
+};
